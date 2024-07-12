d2b0b43f5e53c72f573dd7fbb1280e51a9150d23
Hyun Soo An <ahs20103@gmail.com>
2024-07-12 17:34:23 +0900

    20240712_Data

--- Assets/Scripts/Data/Data.Contents.cs
+++ Assets/Scripts/Data/Data.Contents.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Protobuf.Protocol;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,4 +30,43 @@ namespace Data
 		}
 	}
 #endregion
+<<<<<<< Updated upstream
+=======
+
+#region Skill
+    [Serializable]
+    public class Skill
+    {
+        public int id;
+        public string name;
+        public float cooldown;
+        public int damage;
+        public SkillType skillType;
+        public ProjectileInfo projectileInfo;
+    }
+
+    public class ProjectileInfo
+    {
+        public string name;
+        public float speed;
+        public int range;
+        public string prefab;
+    }
+
+
+    [Serializable]
+    public class SkillData : ILoader<int, Skill>
+    {
+        public List<Skill> skills = new List<Skill>();
+
+        public Dictionary<int, Skill> MakeDict()
+        {
+            Dictionary<int, Skill> dict = new Dictionary<int, Skill>();
+            foreach (Skill Skill in skills)
+                dict.Add(Skill.id, Skill);
+            return dict;
+        }
+    }
+ #endregion
+>>>>>>> Stashed changes
 }
